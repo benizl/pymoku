67bd9f706b6bfaf9c4d9cc5b5d3033c0d3a23677
Ben Nizette <benjamin.nizette@anu.edu.au>
2015-12-10 12:05:18 +1100

    Add example scripts to update bitstreams and firmware

---
+++ examples/update_firmware.py
@@ -0,0 +1,20 @@
+from pymoku import Moku
+import sys, logging
+
+logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s::%(message)s')
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+if len(sys.argv) != 3:
+	print "Usage %s <ip> <file>" % sys.argv[0]
+	exit(1)
+
+# Use Moku.get_by_serial() or get_by_name() if you don't know the IP
+m = Moku(sys.argv[1])
+
+try:
+	m.load_firmware(sys.argv[2])
+except:
+	log.exception("Update Failed")
+finally:
+	m.close()

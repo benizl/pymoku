f0b292073f3340f63d0f30770d97d51889efc6c8
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-01-18 15:45:55 +1100

    Data Parser: Add example LI to CSV conversion script

---
+++ examples/li_to_csv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import pymoku
+
+if len(sys.argv) != 3:
+	print "Usage: li_to_csv.py infile.li outfile.csv"
+	exit(1)
+
+reader = pymoku.dataparser.LIDataFileReader(sys.argv[1])
+reader.to_csv(sys.argv[2])

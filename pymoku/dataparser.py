5bcdb468b05b212a1828d32171f83181ca4e010f
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-01-20 16:23:10 +1100

    Data Parser: Document facilities

--- pymoku/dataparser.py
+++ pymoku/dataparser.py
@@ -9,11 +9,69 @@ class InvalidFormatException(Exception): pass
 class InvalidFileException(Exception): pass
 
 class LIDataFileReader(object):
+	"""
+	Reads LI format data files.
+
+	Data is read from this object in the form of Records. The form of a record depends on the
+	instrument with which the data was captured and is decribed by the :any:`headers` field.
+	A record may be a scalar in the case of, say, a datalogger; or may be a tuple of elements.
+	The Phasemeter for example has records defined as tuples of (frequency, phase, amplitude).
+
+	If multiple channels of data have been recorded, all read methods return time-aligned
+	samples from all channels at once. A single channel capture will return single-element
+	lists of records.
+
+	Presents the iterator interface and the context manager interface.  For example:
+
+	with LIDataFileReader('input.li') as f:
+		for record in f:
+			do_something(record)
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.headers
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.nch
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.instr
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.instrv
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.deltat
+
+	:autoinstanceattribute:: pymoku.dataparser.LIDataFileReader.starttime
+
+	"""
+
 	def __init__(self, filename):
+		"""
+
+		:raises :any:`InvalidFileException`: when file is corrupted or of the wrong version.
+		:type filename: str
+		:param filename: Input filename
+		"""
 		self.records = []
 		self.cal = []
 		self.file = open(filename, 'rb')
 		f = self.file
+
+		# Pre-define instance fields so we can attach docstrings.
+
+		#: Column headers describing the contents of the datafile records
+		self.headers = []
+
+		#: Number of (valid) channels in input file
+		self.nch = 0
+
+		#: Numeric Instrument identifier
+		self.instr = 0
+
+		#: Instrument version
+		self.instrv = 0
+
+		#: Time step with which the data was captured
+		self.deltat = 0
+
+		#: Time at which the recording was started (seconds since Jan 1 1970)
+		self.starttime = 0
 
 		if f.read(2) != 'LI':
 			raise InvalidFileException("Bad Magic")
@@ -90,6 +148,9 @@ class LIDataFileReader(object):
 		return True
 
 	def read(self):
+		""" Read a single record from the file
+		:returns: [ch1_record, ...]
+		"""
 		while not all([ len(r) >= 1 for r in self.records]):
 			if not self._process_chunk():
 				break
@@ -105,6 +166,10 @@ class LIDataFileReader(object):
 		return rec
 
 	def readall(self):
+		""" Returns an array containing all the data from the file.
+
+		Be aware that this can be very big in the case of long or high-rate
+		captures. """
 		ret = []
 
 		for rec in self:
@@ -113,13 +178,16 @@ class LIDataFileReader(object):
 		return ret
 
 	def close(self):
+		""" Safely close the file"""
 		self.file.close()
 
 	def to_csv(self, fname):
-		print "To CSV"
+		""" Dump the contents of this data file as a CSV.
+
+		:param fname: Output CSV filename.
+		"""
 		try: os.remove(fname)
 		except OSError: pass
-		print "Chunk start"
 		# Don't actually care about the chunk contents, just that it's been loaded
 		while self._parse_chunk() is not None:
 			print len(self.parser.processed[0])
@@ -146,7 +214,23 @@ class LIDataFileReader(object):
 
 
 class LIDataFileWriter(object):
+	""" Eases the creation of LI format data files."""
 	def __init__(self, filename, instr, instrv, nch, binstr, procstr, fmtstr, hdrstr, calcoeffs, timestep, starttime):
+		""" Create object and write the header information.
+		Not designed for general use, is likely to only be of utility in the Moku:Lab firmware.
+
+		:param filename: Output file name
+		:param instr: Numeric instrument identifier
+		:param instrv: Numberic instrument version
+		:param nch: Number of valid channels being recorded
+		:param binstr: Format string representing the binary data from the instrument
+		:param procstr: String representing the record processing to apply to the data
+		:param fmtstr: Format string describing the transformation from data records to CSV output
+		:param hdrstr: Format string describing the header lines on a CSV output
+		:param calcoeffs: Array of calibration coefficients for the data being acquired
+		:param timestep: Time between records being captured
+		:param starttime: Time at which the record was started, seconds since Jan 1 1970
+		"""
 		self.file = open(filename, 'wb')
 
 		self.file.write('LI1')
@@ -164,10 +248,18 @@ class LIDataFileWriter(object):
 		self.file.write(hdr)
 
 	def add_data(self, data, ch):
+		""" Append a data chunk to the open file.
+
+		:param data: Bytestring of new data
+		:param ch: Channel number to which the data belongs (0-indexed)
+		"""
 		self.file.write(struct.pack("<BH", ch, len(data)))
 		self.file.write(data)
 
 	def finalize(self):
+		"""
+		Save and close the file.
+		"""
 		self.file.close()
 
 	def __enter__(self):
@@ -177,6 +269,11 @@ class LIDataFileWriter(object):
 		self.finalize()
 
 class LIDataParser(object):
+	""" Backend class that parses raw bytestrings from the instruments according to given format strings.
+
+	Unlikely to be of utility outside the Moku:Lab firmware, an end-user probably wants to instantiate
+	an :any:`LIDataFileReader` instead."""
+
 	@staticmethod
 	def _parse_binstr(binstr):
 		fmt = []
@@ -335,6 +432,7 @@ class LIDataParser(object):
 
 
 	def dump_csv(self, fname=None):
+		""" Write out incremental CSV output from new data"""
 		n_formatted = self._format_records()
 		self.clear_processed(n_formatted)
 
@@ -349,6 +447,10 @@ class LIDataParser(object):
 		self.dout = ''
 
 	def clear_processed(self, _len=None):
+		""" Flush processed data.
+
+		Called by the data consumer to indicate that it's no longer of use (e.g. has been
+		written to a file or otherwise processed)."""
 		if _len is None:
 			self.processed = [[], []]
 		else:
@@ -413,5 +515,9 @@ class LIDataParser(object):
 
 
 	def parse(self, data, ch):
+		""" Parse a chunk of data.
+
+		:param data: bytestring of new data
+		:param ch: Channel to which the data belongs"""
 		self._parse(data, ch)
 		self._process_records()

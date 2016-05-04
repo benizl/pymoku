e8e9d3b3e9fc8751c10d72773c6cb8c6a1d106e3
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-05-04 14:51:10 +1000

    Siggen: Fix register accessors for new layout

--- pymoku/_siggen.py
+++ pymoku/_siggen.py
@@ -48,8 +48,9 @@ REG_SG_MODA2		= 122
 
 SG_WAVE_SINE		= 0
 SG_WAVE_SQUARE		= 1
-SG_WAVE_NOISE		= 2
-SG_WAVE_CLIPSINE	= 3
+SG_WAVE_TRIANGLE	= 2
+SG_WAVE_PULSE		= 3
+SG_WAVE_DC			= 4
 
 SG_MOD_NONE			= 0
 SG_MOD_AMPL			= 1
@@ -62,9 +63,10 @@ SG_MODSOURCE_DAC	= 2
 
 _SG_FREQSCALE		= 1e9 / 2**48
 _SG_PHASESCALE		= 1.0 / (2**32 - 1)
-_SG_RISESCALE		= 1.0 / 2**48
+_SG_RISESCALE		= 1e9 / 2**48
 _SG_AMPSCALE		= 4.0 / 2**16
 _SG_DEPTHSCALE		= 1.0 / 2**15
+_SG_MAX_RISE		= 1e9 - 1
 
 class SignalGenerator(MokuInstrument):
 	"""
@@ -105,7 +107,7 @@ class SignalGenerator(MokuInstrument):
 		self.out1_frequency = 0
 		self.out2_frequency = 0
 
-	def synth_sinewave(self, ch, amplitude, frequency, offset=0, clip=None):
+	def synth_sinewave(self, ch, amplitude, frequency, offset=0):
 		""" Generate a Sine Wave with the given parameters on the given channel.
 
 		:type ch: int
@@ -118,30 +120,19 @@ class SignalGenerator(MokuInstrument):
 		:param frequency: Freqency of the wave
 
 		:type offset: float, volts
-		:param offset: DC offset applied to the waveform
-
-		:type clip: float, 0-1
-		:param clip: Fraction of the waveform to scale to the amplitude. Sine waves with high clipping ratios can be used to generate
-			high-quality, high-speed square waves where a normal square would suffer from edge jitter.  If the clipping ratio is
-			greater than 1,	the amplitude field refers to the size of the clipped output waveform.
-
-			Only integer ratios are allowed, others will be rounded (i.e. *1 / n* for integer *n*).
-
-			*clip = None* behaves the same as *clip = 1* and preserves the entire waveform."""
+		:param offset: DC offset applied to the waveform"""
 		if ch == 1:
-			self.out1_waveform = SG_WAVE_SINE if clip is None else SG_WAVE_CLIPSINE
+			self.out1_waveform = SG_WAVE_SINE
 			self.out1_enable = True
 			self.out1_amplitude = amplitude
 			self.out1_frequency = frequency
 			self.out1_offset = offset
-			self.out1_amp_pc = clip or 1
 		elif ch == 2:
-			self.out2_waveform = SG_WAVE_SINE if clip is None else SG_WAVE_CLIPSINE
+			self.out2_waveform = SG_WAVE_SINE
 			self.out2_enable = True
 			self.out2_amplitude = amplitude
 			self.out2_frequency = frequency
 			self.out2_offset = offset
-			self.out2_amp_pc = clip or 1
 		else:
 			raise ValueOutOfRangeException("Invalid Channel")
 
@@ -180,24 +171,26 @@ class SignalGenerator(MokuInstrument):
 			self.out1_amplitude = amplitude
 			self.out1_frequency = frequency
 			self.out1_offset = offset
+			self.out1_clipsine = False # TODO: Should switch to clip depending on freq or user
 
 			# This is overdefined, but saves the FPGA doing a tricky division
 			self.out1_t0 = risetime
 			self.out1_t1 = duty
 			self.out1_t2 = duty + falltime
-			self.out1_riserate = frequency / risetime
-			self.out1_fallrate = frequency / falltime
+			self.out1_riserate = frequency / risetime if risetime else _SG_MAX_RISE
+			self.out1_fallrate = frequency / falltime if falltime else _SG_MAX_RISE
 		elif ch == 2:
 			self.out2_waveform = SG_WAVE_SQUARE
 			self.out2_enable = True
 			self.out2_amplitude = amplitude
 			self.out2_frequency = frequency
 			self.out2_offset = offset
+			self.out2_clipsine = False
 			self.out2_t0 = risetime
 			self.out2_t1 = duty
 			self.out2_t2 = duty + falltime
-			self.out2_riserate = frequency / risetime
-			self.out2_fallrate = frequency / falltime
+			self.out2_riserate = frequency / risetime if risetime else _SG_MAX_RISE
+			self.out2_fallrate = frequency / falltime if falltime else _SG_MAX_RISE
 		else:
 			raise ValueOutOfRangeException("Invalid Channel")
 
@@ -256,10 +249,16 @@ _siggen_reg_hdl = [
 											lambda rval: rval & 1),
 	('out2_enable',		REG_SG_WAVEFORMS,	lambda s, old: (old & ~2) | int(s) << 1 if int(s) in [0, 1] else None,
 											lambda rval: (rval & 2) >> 1),
-	('out1_waveform',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x70) | (s << 4) if s in [SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_NOISE, SG_WAVE_CLIPSINE] else None,
+	('out1_waveform',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x70) | (s << 4) if s in
+												[SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE, SG_WAVE_DC, SG_WAVE_PULSE] else None,
 											lambda rval: (rval & 0x70) >> 4),
-	('out2_waveform',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x380) | (s << 7) if s in [SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_NOISE, SG_WAVE_CLIPSINE] else None,
-											lambda rval: (rval & 0x380) >> 7),
+	('out2_waveform',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x700) | (s << 8) if s in
+												[SG_WAVE_SINE, SG_WAVE_SQUARE, SG_WAVE_TRIANGLE, SG_WAVE_DC, SG_WAVE_PULSE] else None,
+											lambda rval: (rval & 0x700) >> 8),
+	('out1_clipsine',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x80) | (int(s) << 7) if int(s) in [0, 1] else None,
+											lambda rval: (rval & 0x80) >> 7),
+	('out2_clipsine',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x800) | (int(s) << 11) if int(s) in [0, 1] else None,
+											lambda rval: (rval & 0x800) >> 11),
 	('out1_modulation',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0x00FF0000) | s << 16 if s in range(SG_MOD_NONE, SG_MOD_AMPL | SG_MOD_FREQ | SG_MOD_PHASE) else None,
 											lambda rval: (rval & 0x00FF0000) >> 16),
 	('out2_modulation',	REG_SG_WAVEFORMS,	lambda s, old: (old & ~0xFF000000) | s << 24 if s in range(SG_MOD_NONE, SG_MOD_AMPL | SG_MOD_FREQ | SG_MOD_PHASE) else None,
@@ -279,10 +278,10 @@ _siggen_reg_hdl = [
 	('out1_amplitude',	REG_SG_AMP1,		lambda a, old: _usgn(a / _SG_AMPSCALE, 32), lambda rval: _SG_AMPSCALE * rval),
 	('out2_amplitude',	REG_SG_AMP2,		lambda a, old: _usgn(a / _SG_AMPSCALE, 32), lambda rval: _SG_AMPSCALE * rval),
 	('mod1_frequency',	(REG_SG_MODF1_H, REG_SG_MODF1_L),
-											lambda f, old: ((old & 0xFFFF0000) | _usgn(f / _SG_FREQSCALE, 48) >> 32, _usgn(f / _SG_FREQSCALE, 48) & 0xFFFFFFFF),
+											lambda f, old: ((old[0] & 0xFFFF0000) | _usgn(f / _SG_FREQSCALE, 48) >> 32, _usgn(f / _SG_FREQSCALE, 48) & 0xFFFFFFFF),
 											lambda rval: _SG_FREQSCALE * (rval[0] << 32 | rval[1])),
 	('mod2_frequency',	(REG_SG_MODF2_H, REG_SG_MODF2_L),
-											lambda f, old: ((old & 0xFFFF0000) | _usgn(f / _SG_FREQSCALE, 48) >> 32, _usgn(f / _SG_FREQSCALE, 48) & 0xFFFFFFFF),
+											lambda f, old: ((old[0] & 0xFFFF0000) | _usgn(f / _SG_FREQSCALE, 48) >> 32, _usgn(f / _SG_FREQSCALE, 48) & 0xFFFFFFFF),
 											lambda rval: _SG_FREQSCALE * (rval[0] << 32 | rval[1])),
 	('mod1_offset',		REG_SG_MODF1_H,		lambda o, old: (old & 0x0000FFFF) | _sgn(o, 16) << 16,
 											lambda rval: rval >> 16),
@@ -312,8 +311,8 @@ _siggen_reg_hdl = [
 	('out2_fallrate',	(REG_SG_RFRATE2_H, REG_SG_FALLRATE2_L),
 											lambda r, old: (old[0] & 0x0000FFFF | (_usgn(r / _SG_RISESCALE, 48) >> 32) << 16, _usgn(r / _SG_RISESCALE, 48) & 0xFFFFFFFF),
 											lambda rval: ((rval[0] & 0xFFFF0000) << 16 | rval[1]) * _SG_RISESCALE),
-	('mod1_amplitude',	REG_SG_MODA1,		lambda a, old: _usgn(a / _SG_DEPTHSCALE, 15), lambda rval: rval),
-	('mod2_amplitude',	REG_SG_MODA2,		lambda a, old: _usgn(a / _SG_DEPTHSCALE, 15),	lambda rval: rval),
+	('mod1_amplitude',	REG_SG_MODA1,		lambda a, old: _usgn(a / _SG_DEPTHSCALE, 32), lambda rval: rval * _SG_DEPTHSCALE),
+	('mod2_amplitude',	REG_SG_MODA2,		lambda a, old: _usgn(a / _SG_DEPTHSCALE, 32),	lambda rval: rval * _SG_DEPTHSCALE),
 	('out1_modsource',	REG_SG_MODSOURCE,	lambda s, old: old & ~0x00000006 | s << 1 if s in [SG_MODSOURCE_INT, SG_MODSOURCE_ADC, SG_MODSOURCE_DAC] else None,
 											lambda rval: (rval & 0x00000006) >> 1),
 	('out2_modsource',	REG_SG_MODSOURCE,	lambda s, old: old & 0x00000018 | s << 3 if s in [SG_MODSOURCE_INT, SG_MODSOURCE_ADC, SG_MODSOURCE_DAC] else None,

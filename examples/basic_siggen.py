7311b134379087dd7f181682b9b58aeb8d794e6c
Ben Nizette <benjamin.nizette@anu.edu.au>
2016-05-04 14:51:27 +1000

    Siggen: Tidy up basic siggen example

--- examples/basic_siggen.py
+++ examples/basic_siggen.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s::%(message)s')
 logging.getLogger('pymoku').setLevel(logging.DEBUG)
 
 # Use Moku.get_by_serial() or get_by_name() if you don't know the IP
-m = Moku.get_by_name("Aqua")
+m = Moku.get_by_name("example")
 
 i = m.discover_instrument()
 
@@ -22,19 +22,10 @@ else:
 
 i.set_defaults()
 
-i.synth_sinewave(1, 1.0, 1000000)
-i.synth_squarewave(2, 1.0, 2000000, risetime=0.1, falltime=0.1, duty=0.3)
-i.commit()
-
 try:
-	while True:
-		try:
-			i.out1_offset += 0.05
-		except ValueOutOfRangeException:
-			i.out1_offset = -1
-
-		print i.out1_offset
-
-		i.commit()
+	i.synth_sinewave(1, 1.0, 1000000)
+	i.synth_squarewave(2, 1.0, 2000000, risetime=0.1, falltime=0.1, duty=0.3)
+	i.synth_modulate(1, SG_MOD_AMPL, SG_MODSOURCE_INT, 1, 10)
+	i.commit()
 finally:
 	m.close()
